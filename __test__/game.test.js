d22a6504a009866dd367239f45dba4d0952b2ea4
gabrielesco <41301624+gabrielesco@users.noreply.github.com>
2021-06-03 12:25:56 -0500

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -4,13 +4,9 @@ const fs = require('fs')
 describe('App', () => {
   it('Contains the compiled JavaScript', async (done) => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      try{
         expect(err).toBe(null)
         expect(data).toMatchSnapshot()
         done()
-      }catch (error) {
-        done(error)
-      }
     })
   })
 })
